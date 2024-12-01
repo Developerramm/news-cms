2da4b24ef3236bd8a0c79a97a7f37ab431b42f33
ram kumar <rr697144@gmail.com>
2024-12-01 18:35:55 +0530

    delete post

--- admin/post.php
+++ admin/post.php
@@ -58,8 +58,11 @@
                                     <td><?= $row['category_name'] ?></td>
                                     <td><?= $row['post_date'] ?></td>
                                     <td><?= $row['username']  ?></td>
-                                    <td class='edit'><a href='update-post.php?id=<?= $row['post_id'] ?>'><i class='fa fa-edit'></i></a></td>
-                                    <td class='delete'><a href='delete-post.php?id=<?= $row['post_id'] ?>'><i class='fa fa-trash-o'></i></a></td>
+                                    <td class='edit'><a href='update-post.php?id=<?= $row['post_id'];?>'><i class='fa fa-edit'></i></a></td>
+                                    <td class='delete'>
+                                        <a href='delete-post.php?id=<?= $row['post_id'];?>&catid=<?= $row['category'] ?>'>
+                                            
+                                        <i class='fa fa-trash-o'></i></a></td>
                                 </tr>
                             <?php } ?>
                         </tbody>
